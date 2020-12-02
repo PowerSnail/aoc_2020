7ed40d21c20a3629257ec1204026d61b6f6468a9
PowerSnail <hj@powersnail.com>
2020-12-02 09:09:04 -0800

    use generic line parse

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use clap::clap_app;
-use std::io::{self, BufRead};
+use std::{str::FromStr, io::{self, BufRead}};
 
-fn read_numbers() -> Vec<i32> {
+
+fn read_lines<T>() -> Vec<T>
+    where T : FromStr 
+{
     let stdin = io::stdin();
-    let mut numbers: Vec<i32> = Vec::new();
+    let mut numbers: Vec<T> = Vec::new();
     for line in stdin.lock().lines() {
-        numbers.push(line.unwrap().parse().unwrap())
+        numbers.push(line.unwrap().parse().unwrap_or_else(|_| panic!("Parse Failed!")));
     }
     return numbers;
-}
+} 
 
 fn day1_part1() -> Option<i32> {
-    let mut numbers = read_numbers();
+    let mut numbers: Vec<i32> = read_lines();
     numbers.sort();
 
     let mut lo: usize = 0;
@@ -31,7 +34,7 @@ fn day1_part1() -> Option<i32> {
 }
 
 fn day1_part2() -> Option<i32> {
-    let mut numbers: Vec<i32> = read_numbers();
+    let mut numbers: Vec<i32> = read_lines();
     numbers.sort();
 
     for lo in 0..numbers.len() - 1 {
