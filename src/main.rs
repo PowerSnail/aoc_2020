edf422c8591c37c703f60073cbc99ece9d308293
PowerSnail <hj@powersnail.com>
2020-12-02 09:35:47 -0800

    day2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::clap_app;
 use std::{str::FromStr, io::{self, BufRead}};
+use regex::Regex;
 
 
 fn read_lines<T>() -> Vec<T>
@@ -49,16 +50,55 @@ fn day1_part2() -> Option<i32> {
     None
 }
 
+fn day2_part1() -> Option<i32> {
+    let stdin = io::stdin();
+    let password_rule_re = Regex::new(r"(\d+)-(\d+) ([a-zA-Z0-9_]): ([a-zA-Z0-9_]*)").unwrap();
+    let mut count_valid = 0;
+    for line in stdin.lock().lines() {
+        let line = &line.unwrap();
+        let capture = password_rule_re.captures(line).unwrap();
+        let target = capture[3].as_bytes()[0];
+        let letter_count = capture[4].as_bytes().into_iter().filter(|&&x| x == target).count();
+        if capture[1].parse::<usize>().unwrap() <= letter_count && letter_count <= capture[2].parse::<usize>().unwrap() {
+            count_valid += 1;
+        }
+    }
+    Some(count_valid)
+}
+
+fn day2_part2() -> Option<i32> {
+    let stdin = io::stdin();
+    let password_rule_re = Regex::new(r"(\d+)-(\d+) ([a-zA-Z0-9_]): ([a-zA-Z0-9_]*)").unwrap();
+    let mut count_valid = 0;
+    for line in stdin.lock().lines() {
+        let line = &line.unwrap();
+        let capture = password_rule_re.captures(line).unwrap();
+        let i: usize = capture[1].parse::<usize>().unwrap() - 1;
+        let j: usize = capture[2].parse::<usize>().unwrap() - 1;
+        let target = capture[3].as_bytes()[0];
+        let password = capture[4].as_bytes();
+        
+        if (password[i] == target) ^ (password[j] == target) {
+            count_valid += 1;
+        }
+    }
+    Some(count_valid)
+}
+
 fn main() {
     let matches = clap_app!(myapp =>
         (@setting SubcommandRequiredElseHelp)
         (@subcommand "day1-part1" =>)
         (@subcommand "day1-part2" =>)
+        (@subcommand "day2-part1" =>)
+        (@subcommand "day2-part2" =>)
     ).get_matches();
 
     let answer = match matches.subcommand() {
         Some(("day1-part1", _)) => day1_part1(),
         Some(("day1-part2", _)) => day1_part2(),
+        Some(("day2-part1", _)) => day2_part1(),
+        Some(("day2-part2", _)) => day2_part2(),
         _ => unreachable!(),
     };
 
