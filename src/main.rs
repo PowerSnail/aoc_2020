9710a6a0305d8d48435fb6779954c4bb76fa27d2
PowerSnail <hj@powersnail.com>
2020-12-02 22:48:15 -0800

    day3

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ fn read_lines<T>() -> Vec<T>
     return numbers;
 } 
 
-fn day1_part1() -> Option<i32> {
-    let mut numbers: Vec<i32> = read_lines();
+fn day1_part1() -> Option<i64> {
+    let mut numbers: Vec<i64> = read_lines();
     numbers.sort();
 
     let mut lo: usize = 0;
@@ -34,8 +34,8 @@ fn day1_part1() -> Option<i32> {
     None
 }
 
-fn day1_part2() -> Option<i32> {
-    let mut numbers: Vec<i32> = read_lines();
+fn day1_part2() -> Option<i64> {
+    let mut numbers: Vec<i64> = read_lines();
     numbers.sort();
 
     for lo in 0..numbers.len() - 1 {
@@ -50,7 +50,7 @@ fn day1_part2() -> Option<i32> {
     None
 }
 
-fn day2_part1() -> Option<i32> {
+fn day2_part1() -> Option<i64> {
     let stdin = io::stdin();
     let password_rule_re = Regex::new(r"(\d+)-(\d+) ([a-zA-Z0-9_]): ([a-zA-Z0-9_]*)").unwrap();
     let mut count_valid = 0;
@@ -66,7 +66,7 @@ fn day2_part1() -> Option<i32> {
     Some(count_valid)
 }
 
-fn day2_part2() -> Option<i32> {
+fn day2_part2() -> Option<i64> {
     let stdin = io::stdin();
     let password_rule_re = Regex::new(r"(\d+)-(\d+) ([a-zA-Z0-9_]): ([a-zA-Z0-9_]*)").unwrap();
     let mut count_valid = 0;
@@ -85,6 +85,45 @@ fn day2_part2() -> Option<i32> {
     Some(count_valid)
 }
 
+fn day3_part1() -> Option<i64> {
+    let stdin = io::stdin();
+    let mut x : usize = 3;
+    let mut tree_count = 0;
+    for line in stdin.lock().lines().skip(1) {
+        let line = &line.unwrap();
+        let line = line.as_bytes();
+        let width = line.len();
+        let x_ = x % width;
+        if line[x_] == '#' as u8 {
+            tree_count += 1;
+        }
+        x += 3;
+    }
+    Some(tree_count)
+}
+
+
+fn day3_part2() -> Option<i64> {
+    let stdin = io::stdin();
+    let rights = vec![1, 3, 5, 7, 1];
+    let downs = vec![1, 1, 1, 1, 2];
+    let mut tree_counts = vec![0, 0, 0, 0, 0];
+
+    for (row, line) in stdin.lock().lines().enumerate().skip(1) {
+        let line = &line.unwrap();
+        let line = line.as_bytes();
+        let width = line.len();
+
+        for i in 0..5 {
+            if row % downs[i] == 0 && line[rights[i] * row / downs[i] % width] == '#' as u8 {
+                tree_counts[i] += 1;
+            }
+        }
+    }
+    println!("{:#?}", tree_counts);
+    Some(tree_counts[0] * tree_counts[1] * tree_counts[2] * tree_counts[3] * tree_counts[4])
+}
+
 fn main() {
     let matches = clap_app!(myapp =>
         (@setting SubcommandRequiredElseHelp)
@@ -92,6 +131,8 @@ fn main() {
         (@subcommand "day1-part2" =>)
         (@subcommand "day2-part1" =>)
         (@subcommand "day2-part2" =>)
+        (@subcommand "day3-part1" =>)
+        (@subcommand "day3-part2" =>)
     ).get_matches();
 
     let answer = match matches.subcommand() {
@@ -99,6 +140,8 @@ fn main() {
         Some(("day1-part2", _)) => day1_part2(),
         Some(("day2-part1", _)) => day2_part1(),
         Some(("day2-part2", _)) => day2_part2(),
+        Some(("day3-part1", _)) => day3_part1(),
+        Some(("day3-part2", _)) => day3_part2(),
         _ => unreachable!(),
     };
 
